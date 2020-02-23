b0960f34513e3c92737b71397df1022fbc6c88e6
Principal Charlotte <pcharlot@il-k6.21-school.ru>
2020-02-23 20:01:30 +0300

    dsdsds

---
+++ operations.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operations.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pcharlot <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/16 14:28:22 by pcharlot          #+#    #+#             */
+/*   Updated: 2020/02/19 12:48:47 by pcharlot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+//void	sa(t_stack **st_a) ///swap a - swap the first 2 elements at the top of stack a. Do nothing if there is only one or no elements).
+//{
+//	t_stack	*temp;
+//
+//	if (*st_a && (*st_a)->next)
+//	{
+//		temp = *st_a;
+//		*st_a = temp->next;
+//		temp->next = (*st_a)->next;
+//		(*st_a)->next = temp;
+//	}
+//}
+//
+//void	sb(t_stack **st_b) ///swap b - swap the first 2 elements at the top of stack b. Do nothing if there is only one or no elements).
+//{
+//	t_stack	*temp;
+//
+//	if (*st_b && (*st_b)->next)
+//	{
+//		temp = *st_b;
+//		*st_b = temp->next;
+//		temp->next = (*st_b)->next;
+//		(*st_b)->next = temp;
+//	}
+//}
+
+//void	pa(t_stack **st_a, t_stack **st_b) ///pa : push a - take the first element at the top of b and put it at the top of a. Do nothing if b is empty.
+//{
+//	if(*st_b)
+//	{
+//		add_new(st_a, (*st_b)->nbr);
+//		dell_top(st_b);
+//	}
+//}
+
+//void	pb(t_stack **st_b, t_stack **st_a) ///pb : push b - take the first element at the top of a and put it at the top of b. Do nothing if a is empty.
+//{
+//	if(*st_a)
+//	{
+//		add_new(st_b, (*st_a)->nbr);
+//		dell_top(st_a);
+//	}
+//}
+
+//void	ra(t_stack **st_a) ///ra : rotate a - shift up all elements of stack a by 1. The first element becomes the last one.
+//{
+//	t_stack	*temp;
+//
+//	if(*st_a && (*st_a)->next)
+//	{
+//		temp = *st_a;
+//		add_new_down(st_a, (*st_a)->nbr);
+//		dell_top(st_a);
+//	}
+//}
+
+//void	rb(t_stack **st_b) ///rb : rotate b - shift up all elements of stack b by 1. The first element becomes the last one.
+//{
+//	if(*st_b && (*st_b)->next)
+//	{
+//		add_new_down(st_b, (*st_b)->nbr);
+//		dell_top(st_b);
+//	}
+//}
+
+//void	rra(t_stack **st_a) ///reverse rotate a - shift down all elements of stack a by 1. The last element becomes the first one.
+//{
+//	t_stack	*temp;
+//
+//	if (*st_a && (*st_a)->next)
+//	{
+//		temp = *st_a;
+//		while (temp->next)
+//			temp = temp->next;
+//		add_new(st_a, temp->nbr);
+//		dell_down(st_a);
+//	}
+//}
+
+//void	rrb(t_stack **st_b) ///reverse rotate b - shift down all elements of stack b by 1. The last element becomes the first one.
+//{
+//	t_stack	*temp;
+//
+//	if (*st_b && (*st_b)->next)
+//	{
+//		temp = *st_b;
+//		while (temp->next)
+//			temp = temp->next;
+//		add_new(st_b, temp->nbr);
+//		dell_down(st_b);
+//	}
+//}
---
+++ stacks.c
@@ -0,0 +1,234 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stacks.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pcharlot <pcharlot@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/16 14:29:37 by pcharlot          #+#    #+#             */
+/*   Updated: 2020/02/23 17:33:33 by pcharlot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+
+
+void	add_new(t_stack **st, int digit)
+{
+	t_stack	*new;
+
+	new = (t_stack *)malloc(sizeof(t_stack));
+	ft_fatality(new);
+/*	new->nbr = malloc(sizeof(int)); */ ///это нужно ???
+	new->nbr = digit;
+	new->next = *st;
+	*st = new;
+}
+
+void	dell_top(t_stack **st)
+{
+	t_stack *temp;
+
+	if (*st)
+	{
+		temp = *st;
+		*st = (*st)->next;
+		free(temp);
+	}
+}
+
+void	add_new_down(t_stack **st, int digit)
+{
+	t_stack	*temp;
+//	t_stack *prev;
+	t_stack	*new;
+
+	new = (t_stack *)malloc(sizeof(t_stack));
+	ft_fatality(new);
+	temp = *st;
+	new->nbr = digit;
+	new->next = NULL;
+
+	while(temp->next)
+	{
+//		prev = temp;
+		temp = temp->next;
+	}
+	temp->next = new;
+}
+
+void	dell_down(t_stack **st)
+{
+	t_stack	*temp;
+	t_stack *prev;
+
+	if (*st)
+	{
+		if ((*st)->next)
+		{
+			temp = *st;
+			prev = *st;
+			while (temp->next) {
+				prev = temp;
+				temp = temp->next;
+			}
+			prev->next = NULL;
+			free(temp);
+		}
+		else
+			{
+			free(*st);
+			*st = NULL; ///без этой строки ну просто трындец как все ломается =(
+			}
+
+	}
+}
+
+void	nachalo_detected(int *tab, int size)
+{
+	t_stack *st_a;
+	t_stack *st_b;
+	int i;
+
+//	st_a = (t_stack *)malloc(sizeof(t_stack));
+//	st_a->nbr = tab[0];
+//	st_a->next = NULL;
+
+	st_a = NULL;
+
+//	st_b = (t_stack *)malloc(sizeof(t_stack));
+//	st_b->next = NULL;
+	st_b = NULL;
+//	print_stack(st);
+	i = 0;
+	while (i < size)
+	{
+		add_new(&st_a, tab[i]);
+		++i;
+	}
+	printf("__________________________");
+	printf("\nstack a:\n");
+	print_stack(st_a);
+
+	printf("\nstack b:\n");
+	print_stack(st_b);
+	printf("__________________________\n\n");
+//	dell_top(&st_a);
+//	printf("\nDelTopA:\n");
+//	print_stack(st_a);
+
+//	printf("\nstack b:\n");
+//	print_stack(st_b);
+
+//	dell_top(&st_a);
+//	printf("\nDelTopA:\n");
+//	print_stack(st_a);
+//
+//	dell_top(&st_a);
+//	printf("\nDelTopA:\n");
+//	print_stack(st_a);
+//
+//	dell_top(&st_a);
+//	printf("\nDelTopA:\n");
+//	print_stack(st_a);
+
+//	sa(&st_a);
+//	printf("\nstack a after sa:\n");
+//	print_stack(st_a);
+
+//	pa(&st_a, &st_b);
+//	printf("\n____pa____");
+//
+//	printf("\nstack a:\n");
+//	print_stack(st_a);
+//
+//	printf("\nstack b:\n");
+//	print_stack(st_b);
+
+//	pb(&st_b, &st_a);
+//	pb(&st_b, &st_a);
+//	printf("\n____pb*2____");
+//	printf("\nstack a:\n");
+//	print_stack(st_a);
+//	printf("\nstack b:\n");
+//	print_stack(st_b);
+//
+//	pa(&st_a, &st_b);
+//	pa(&st_a, &st_b);
+//	printf("\nstack a:\n");
+//	print_stack(st_a);
+//
+//	printf("\nstack b:\n");
+//	print_stack(st_b);
+
+//	add_new_down(&st_a, 1000);
+//	printf("\nstack a after add_new_down:\n");
+//	print_stack(st_a);
+//
+//	add_new(&st_a, -1000);
+//	printf("\nstack a after add_new:\n");
+//	print_stack(st_a);
+
+
+//	ra(&st_a);
+//	printf("\nstack a after ra:\n");
+//	print_stack(st_a);
+
+//	rb(&st_b);
+
+//	dell_down(&st_a);
+//	dell_down(&st_a);
+//	dell_down(&st_a);
+//	dell_down(&st_a);
+//	dell_down(&st_a);
+//	dell_down(&st_a);
+//	dell_down(&st_a);
+//	printf("\nstack a after dell_down:\n");
+//	print_stack(st_a);
+
+
+
+//	dell_top(&st_a);
+//	dell_top(&st_a);
+//	dell_top(&st_a);
+//	dell_top(&st_a);
+//	dell_top(&st_a);
+//	dell_top(&st_a);
+//	printf("\nstack a after dell_top:\n");
+//	print_stack(st_a);
+
+
+//	ra(&st_a);
+//	printf("\nstack a after ra:\n");
+//	print_stack(st_a);
+
+//	rra(&st_a);
+//	printf("\nstack a after rra:\n");
+//	print_stack(st_a);
+
+
+
+//	sa(&st_a);
+//	pb(&st_b, &st_a);
+//	pb(&st_b, &st_a);
+//	pb(&st_b, &st_a);
+//	ra(&st_a);
+//	rb(&st_b);
+//	rra(&st_a);
+//	rrb(&st_b);
+//	sa(&st_a);
+//	pa(&st_a, &st_b);
+//	pa(&st_a, &st_b);
+//	pa(&st_a, &st_b);
+//
+//	printf("\nstack a:\n");
+//	print_stack(st_a);
+//
+//	printf("\nstack b:\n");
+//	print_stack(st_b);
+
+	kuku(tab, size, &st_a, &st_b);
+
+
+}
